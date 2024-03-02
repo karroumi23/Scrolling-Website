c95fe0374ff6618cfa2ede1ac28d731363e7ce63
karroumi <anasskarroumi2016@gmail.com>
2024-03-02 21:42:38 +0100

    get element by id in page js

---
+++ main.js
@@ -0,0 +1,10 @@
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let mountains3 = document.getElementById("mountains3");
+let mountains4 = document.getElementById("mountains4");
+let river = document.getElementById("river");
+let boat = document.getElementById("boat");
+let nouvil = document.querySelector(".nouvil");
+
+
+
